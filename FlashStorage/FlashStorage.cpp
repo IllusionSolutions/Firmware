9276af8a55e3f232b164105db4aecc2816c5f708
AntelMarc <antel.marc@gmail.com>
2016-07-19 12:48:51 +0200

    Implemented store methods. Needs testing
    full() method needs to be implemented first before testing

--- FlashStorage/FlashStorage.cpp
+++ FlashStorage/FlashStorage.cpp
@@ -23,12 +23,43 @@ void FlashStorage::clearMemory()
 
 bool FlashStorage::store(Reading data)
 {
-  return false;
+    if(!full())
+    {
+        Serial.println("Attempting to store");
+        EEPROM.put(last_address, data);
+        last_address = last_address + sizeof(data);
+        return true;
+    }
+    else
+    {
+        Serial.println("Address full");
+        full = true;
+        return false;
+    }
 }
 
 bool FlashStorage::store(float current, float voltage,float power,time_t timeOfReading, int sequence)
 {
-  return false;
+  Reading data;
+  data.current=current;
+  data.voltage=voltage;
+  data.power=power;
+  data.timeOfReading=timeOfReading;
+  data.sequence=sequence;
+  
+    if(!full())
+    {
+        Serial.println("Attempting to store");
+        EEPROM.put(last_address, data);
+        last_address = last_address + sizeof(data);
+        return true;
+    }
+    else
+    {
+        Serial.println("Address full");
+        full = true;
+        return false;
+    }
 }
 
 FlashStorage::Reading FlashStorage::getLast()
